aa03e575c89ae4a9d19977d9f3ad918fdf36d91b
Zian Zhou <zianzhou@zians-mbp.home>
2024-11-22 18:19:59 -0500

    Small refactoring change for backend testing

--- src/game.rs
+++ src/game.rs
@@ -153,3 +153,8 @@ impl Game {
         }
     }
 }
+
+pub fn backend() {
+    let mut game = Game::new();
+    game.play();
+}
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ mod frontend;
 mod game; // Import the backend game logic // Import the frontend logic
 
 fn main() {
-    frontend::main(); // Call the frontend main function
+    // frontend::main(); // Call the frontend main function
+    game::backend();
 }
